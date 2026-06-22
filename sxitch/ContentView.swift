2a3bcb30e91d0419ff7515d625c42e32fc52e4f6
unsecretised <no1umang@gmail.com>
2026-06-22 09:04:36 +0800

    Initial Commit

---
+++ sxitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sxitch
+//
+//  Created by Umang on 22/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sxitch/sxitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  sxitchApp.swift
+//  sxitch
+//
+//  Created by Umang on 22/6/26.
+//
+
+import SwiftUI
+
+@main
+struct sxitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
