9c393f82682a5d72e09550087b6f7d6467ba3e7c
unsecretised <no1umang@gmail.com>
2026-09-16 11:17:24 +0800

    add radial view window picker

--- sxitch/Views/WindowPickerView.swift
+++ sxitch/Views/WindowPickerView.swift
@@ -18,6 +18,21 @@ struct WindowPickerView: View {
     }
 
     var body: some View {
+        if layoutStyle == "circle" {
+            WindowPickerCircleLayout(
+                windows: filtered,
+                appName: appName,
+                appIcon: appIcon,
+                typed: typed,
+                onSelect: onSelect
+            )
+        } else {
+            windowStack
+        }
+    }
+
+    @ViewBuilder
+    private var windowStack: some View {
         VStack(spacing: 0) {
             HStack(spacing: 8) {
                 Image(nsImage: appIcon)
